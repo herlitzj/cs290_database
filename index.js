4781a0b8c23312c70a1313e9a2d6b33a1e625cff
Jeff <herlitzj@gmail.com>
2016-05-24 08:26:23 -0400

    moved style

--- index.js
+++ index.js
@@ -17,8 +17,11 @@ var pool = mysql.createPool({
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.use('views/script.js', express.static(path.join(__dirname, 'views/script.js')));
-app.use('views/style.css', express.static(path.join(__dirname, 'views/style.css')));
+
+app.configure(function(){
+  app.use('views/script.js', express.static(path.join(__dirname, 'views/script.js')));
+  app.use('views/style.css', express.static(path.join(__dirname, 'views/style.css')));
+};
 
 app.get('/',function (req, res){
   var payload = {};
