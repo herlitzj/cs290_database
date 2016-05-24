9f5b28047956d686e996918549303dbf883ddd78
Jeff <herlitzj@gmail.com>
2016-05-24 10:48:23 -0400

    script

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded());
+app.use('/public/scripts', express.static(__dirname + '/public/scripts'));
 app.use('/public/css', express.static(__dirname + '/public/css'));
 
 //DB CONNECTION
