cbc5cbd72e75a9c72ea19ad63101dcf8cfe9f8b6
Jeff <herlitzj@gmail.com>
2016-05-24 11:19:48 -0400

    script

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,14 +1,3 @@
-// window.addEventListener('load', function() {
-//   console.log("SCRIPT LOADED")
-// }, false);
-
-// $(window).load(function() {
-//   function() {
-//     console.log("LOADED");
-//   };
-// });
-
-
 function logYourself() {
   console.log("LOADED");
 };
@@ -36,18 +25,18 @@ window.onload = logYourself;
 // }
 
 
-// function addRow() {
-//   console.log("adding row");
-//   var req = new XMLHttpRequest();
-//   var formData = {
-//     'name': document.getElementById('postName').value,
-//     'reps': document.getElementById('postReps').value,
-//     'weight': document.getElementById('postWeight').value,
-//     'date': document.getElementById('postDate').value,
-//     'lbs': document.getElementById('postLbs').value,
-//   }
-//   var payload = {text: formData};
-//   req.open('POST', '/', true);
-//   req.setRequestHeader('Content-Type', 'application/json');
-//   req.send(JSON.stringify(payload));
-// }
+function addRow() {
+  console.log("adding row");
+  var req = new XMLHttpRequest();
+  var formData = {
+    'name': document.getElementById('postName').value,
+    'reps': document.getElementById('postReps').value,
+    'weight': document.getElementById('postWeight').value,
+    'date': document.getElementById('postDate').value,
+    'lbs': document.getElementById('postLbs').value,
+  }
+  var payload = {text: formData};
+  req.open('POST', '/', true);
+  req.setRequestHeader('Content-Type', 'application/json');
+  req.send(JSON.stringify(payload));
+}
