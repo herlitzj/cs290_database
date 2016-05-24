5dfae38a601b4fa7cad110fd8992bfe616cd1523
Jeff <herlitzj@gmail.com>
2016-05-24 10:08:41 -0400

    script

--- views/script.js
+++ views/script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   console.log("SCRIPT LOADED")
-  document.getElementById("addRow").onclick = addRow();
+  // document.getElementById("addRow").onclick = addRow();
   
 }
 
@@ -26,18 +26,18 @@ window.onload = function() {
 // }
 
 
-function addRow() {
-  console.log("adding row");
-  var req = new XMLHttpRequest();
-  var formData = {
-    'name': document.getElementById('postName').value,
-    'reps': document.getElementById('postReps').value,
-    'weight': document.getElementById('postWeight').value,
-    'date': document.getElementById('postDate').value,
-    'lbs': document.getElementById('postLbs').value,
-  }
-  var payload = {text: formData};
-  req.open('POST', '/', true);
-  req.setRequestHeader('Content-Type', 'application/json');
-  req.send(JSON.stringify(payload));
-}
+// function addRow() {
+//   console.log("adding row");
+//   var req = new XMLHttpRequest();
+//   var formData = {
+//     'name': document.getElementById('postName').value,
+//     'reps': document.getElementById('postReps').value,
+//     'weight': document.getElementById('postWeight').value,
+//     'date': document.getElementById('postDate').value,
+//     'lbs': document.getElementById('postLbs').value,
+//   }
+//   var payload = {text: formData};
+//   req.open('POST', '/', true);
+//   req.setRequestHeader('Content-Type', 'application/json');
+//   req.send(JSON.stringify(payload));
+// }
