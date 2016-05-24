03a20c4b989739d9ca3fc7772d56b5a98817952b
Jeff <herlitzj@gmail.com>
2016-05-24 09:41:08 -0400

    moved logic to button

--- views/script.js
+++ views/script.js
@@ -1,22 +1,39 @@
-document.addEventListener('DOMContentLoaded', bindButtons);
+// document.addEventListener('DOMContentLoaded', bindButtons);
 
 
-function bindButtons(){  
-  console.log("script loaded");
-  document.getElementById('postSubmit').addEventListener('click', function(event) {
-    console.log("POST BUTTON CLICKED");
-    var req = new XMLHttpRequest();
-    var formData = {
-      'name': document.getElementById('postName').value,
-      'reps': document.getElementById('postReps').value,
-      'weight': document.getElementById('postWeight').value,
-      'date': document.getElementById('postDate').value,
-      'lbs': document.getElementById('postLbs').value,
-    }
-    var payload = {text: formData};
-    req.open('POST', '/', true);
-    req.setRequestHeader('Content-Type', 'application/json');
-    req.send(JSON.stringify(payload));
-    event.preventDefault();
-  });
+// function bindButtons(){  
+//   console.log("script loaded");
+//   document.getElementById('postSubmit').addEventListener('click', function(event) {
+//     console.log("POST BUTTON CLICKED");
+//     var req = new XMLHttpRequest();
+//     var formData = {
+//       'name': document.getElementById('postName').value,
+//       'reps': document.getElementById('postReps').value,
+//       'weight': document.getElementById('postWeight').value,
+//       'date': document.getElementById('postDate').value,
+//       'lbs': document.getElementById('postLbs').value,
+//     }
+//     var payload = {text: formData};
+//     req.open('POST', '/', true);
+//     req.setRequestHeader('Content-Type', 'application/json');
+//     req.send(JSON.stringify(payload));
+//     event.preventDefault();
+//   });
+// }
+
+
+function addRow() {
+  console.log("adding row");
+  var req = new XMLHttpRequest();
+  var formData = {
+    'name': document.getElementById('postName').value,
+    'reps': document.getElementById('postReps').value,
+    'weight': document.getElementById('postWeight').value,
+    'date': document.getElementById('postDate').value,
+    'lbs': document.getElementById('postLbs').value,
+  }
+  var payload = {text: formData};
+  req.open('POST', '/', true);
+  req.setRequestHeader('Content-Type', 'application/json');
+  req.send(JSON.stringify(payload));
 }
