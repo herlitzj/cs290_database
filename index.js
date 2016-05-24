7a3dfbc7cbb3bc26cfd7ae8a993096648c147170
Jeff <herlitzj@gmail.com>
2016-05-24 09:22:12 -0400

    moved style

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded());
-app.use(express.static(__dirname + '/views'));
-app.use(express.static(__dirname + '/views/style.css'));
+app.use('/views', express.static(__dirname + '/views'));
+// app.use(express.static(__dirname + '/views/style.css'));
 
 //DB CONNECTION
 var mysql = require('mysql');
